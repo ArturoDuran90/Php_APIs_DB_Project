a1603b23320be37be0c408ce3d32adeb7a68d510
Arturo Duran <78046770+ArturoDuran90@users.noreply.github.com>
2024-06-24 21:57:29 -0600

    Update dbConnector.php

--- Back_End/dbConnector.php
+++ Back_End/dbConnector.php
@@ -1,9 +1,5 @@
 <?php
-DEFINE('DB_USER', 'root');
-DEFINE('DB_PSWD', 'Nu191036673');
-DEFINE('DB_SERVER', 'localhost');
-DEFINE('DB_NAME', 'vehicleinfo');
-
+require "dbProperties.php";
 // ///////////////////////////////////////////////////
 // Get db connection
 function ConmnGet()
